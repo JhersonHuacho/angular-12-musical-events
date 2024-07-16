4c267ac66f6a3d8e468fed0aa869c4d09183a69a
Francisco <jhersonhuacho@gmail.com>
2024-07-16 09:50:00 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-		selector: 'app-root',
-		templateUrl: './app.component.html',
-		styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-		title = 'angular-musical-events-modulos hola';
+	title = 'angular-musical-events-modulos hola';
 }
