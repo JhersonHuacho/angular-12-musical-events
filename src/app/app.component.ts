ad5c3e62863604ab83cb471c5dcaa1f73490bc7d
Francisco <jhersonhuacho@gmail.com>
2024-07-16 10:06:38 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'angular-musical-events-modulos hola';
+	title = 'angular-musical-events-modulos hola dede';
 }
