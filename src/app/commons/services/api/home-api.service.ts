0974d7fc6da994234f80c7f68c76fa305e36c769
Francisco <jhersonhuacho@gmail.com>
2024-07-19 09:23:46 -0500

    configuración env

--- src/app/commons/services/api/home-api.service.ts
+++ src/app/commons/services/api/home-api.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const URL_HOME = environment.host + '/home';
 
 @Injectable({
 	providedIn: 'root'
@@ -9,6 +12,6 @@ export class HomeApiService {
 	constructor(private _httpClient: HttpClient) {}
 
 	getHome(): Observable<Object> {
-		return this._httpClient.get('https://api.example.com/home');
+		return this._httpClient.get(URL_HOME);
 	}
 }
--- src/environments/environment.mock.ts
+++ src/environments/environment.mock.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	production: true,
+	production: false,
 	host: 'https://api.example.mocks/api'
 };
